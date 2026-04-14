5b069111d741633bbf3eae30845fe486d46c4a36
vishnu24naveen <vishnushankar.v94@gmail.com>
2026-04-14 17:50:59 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "canteen-v1";
+const urlsToCache = ["index.html","manifest.json"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
